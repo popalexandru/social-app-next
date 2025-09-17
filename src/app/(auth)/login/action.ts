7fad84ecf378f66f585de67c102f5d754a9be6fc
popalexandru <popalexandru1197@gmail.com>
2025-09-17 13:18:59 +0300

    login logic, no ui yet

---
+++ src/app/(auth)/login/action.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { loginSchema, LoginValues } from "@/lib/validation";
+import { isRedirectError } from "next/dist/client/components/redirect";
+import { verify } from "@node-rs/argon2";
+import { redirect } from "next/navigation";
+
+export async function login(
+  credentials: LoginValues,
+): Promise<{ error: string }> {
+  try {
+    const { username, password } = loginSchema.parse(credentials);
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        username: {
+          equals: username,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (!existingUser || !existingUser.passwordHash) {
+      return {
+        error: "Incorrect username or password",
+      };
+    }
+
+    const validPwd = await verify(existingUser.passwordHash, password, {
+      memoryCost: 19456,
+      timeCost: 2,
+      outputLen: 32,
+      parallelism: 1,
+    });
+
+    return redirect("/");
+  } catch (e) {
+    if (isRedirectError(e)) throw e;
+    console.error(e);
+
+    return {
+      error: "Something went wrong",
+    };
+  }
+}
---
+++ src/lib/validation.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+const requiredString = z.string().trim().min(1, "Required");
+
+export const signUpSchema = z.object({
+  email: requiredString.email("Invalid email address"),
+  username: requiredString.regex(/^[a-zA-Z0-9_-]+$/, "No special characters"),
+  password: requiredString.min(8, "Must be atleast 8 characters"),
+});
+
+export type SignUpValues = z.infer<typeof signUpSchema>;
+
+export const loginSchema = z.object({
+  username: requiredString,
+  password: requiredString,
+});
+
+export type LoginValues = z.infer<typeof loginSchema>;
